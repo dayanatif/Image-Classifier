90db24a5ee636a47dbb343c2a565ef1eaac3a582
Rao Muhammad Dayan Atif <162185730+dayanatif@users.noreply.github.com>
2024-10-11 00:44:46 +0500

    Update apppp.py

--- apppp.py
+++ apppp.py
@@ -4,10 +4,10 @@ import numpy as np
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
 
-# Load the pre-trained MobileNetV2 model
+
 model = MobileNetV2(weights='imagenet')
 
-# Streamlit app layout
+
 st.title("Image Classifier App 📷")
 st.write("Upload an image to classify it!")
 
@@ -15,17 +15,17 @@ st.write("Upload an image to classify it!")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
-    # Open and display the image
+    
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image', use_column_width=True)
     
-    # Preprocess the image
+    
     image = image.resize((224, 224))
     img_array = np.array(image)
     img_array = np.expand_dims(img_array, axis=0)
     img_array = preprocess_input(img_array)
 
-    # Make predictions
+    
     predictions = model.predict(img_array)
     labels = decode_predictions(predictions)
     
